c072fae7734d615a096e7616e61db03dc89d1046
Derek Fermaint <fermaint.derek@gmail.com>
2017-12-26 22:13:03 -0800

    recursive method added

--- factorialize.js
+++ factorialize.js
@@ -3,6 +3,7 @@
  * Author: Derek Fermaint
  */
 
+// using a for loop
  function factorialize(num) {
  	var result = 1;
  	for(var i = num; i > 0; i--){
@@ -10,3 +11,14 @@
  	}
  	return result;
  }
+
+ // using a recursive function
+function factorialize(num) {
+  if(num == 0)
+    return 1;
+  else if (num == 1)
+    return num;
+  else      
+  return num * factorialize(num - 1);
+}
+
--- reverse_string.js
+++ reverse_string.js
@@ -6,7 +6,7 @@
  // without using JS methods
  function reverseString(str){
  	var result = "";
- 	for(var i = str.length - 1; i >= 0; i++){
+ 	for(var i = str.length - 1; i >= 0; i--){
  		result += str[i];
  	}
  	return result;
